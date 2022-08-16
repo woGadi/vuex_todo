328ae9c3832e9eb1d0c028931f8b0828fcdd5f2c
woGadi <myhiawo@163.com>
2022-08-16 09:11:29 +0800

    完成了基于vuex的todo小练习

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,134 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="body-bg">
+    <div id="app">
+      <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="handleInputValue" />
+      <a-button type="primary" @click="addItemToList">添加事项</a-button>
+
+      <!-- dataSource 是列表的数据源 -->
+      <!-- 因为要按需展示列表，所以数据源是 showListByKey；原数据源是 list -->
+      <a-list bordered :dataSource="showListByKey" class="dt_list">
+        <a-list-item slot="renderItem" slot-scope="item">
+          <!-- 复选框 -->
+          <a-checkbox
+            :checked="item.done"
+            @change="
+              (e) => {
+                cbStatusChanged(e, item)
+              }
+            "
+            >{{ item.info }}</a-checkbox
+          >
+          <!-- 删除链接 -->
+          <a slot="actions" @click="removeItemById(item)">删除</a>
+        </a-list-item>
+
+        <!-- footer区域 -->
+        <div slot="footer" class="footer">
+          <!-- 未完成的任务个数 -->
+          <span>{{ unDoneLength }}条剩余</span>
+          <!-- 操作按钮 -->
+          <a-button-group>
+            <!-- 以下按钮用于切换列表视图 -->
+            <a-button :type="viewKey === 'all' ? 'primary' : 'default'" @click="changeList('all')">全部</a-button>
+            <a-button :type="viewKey === 'un_done' ? 'primary' : 'default'" @click="changeList('un_done')">未完成</a-button>
+            <a-button :type="viewKey === 'done' ? 'primary' : 'default'" @click="changeList('done')">已完成</a-button>
+          </a-button-group>
+          <!-- 把已经完成的任务清空 -->
+          <a @click="cleanDone">清除已完成</a>
+        </div>
+      </a-list>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapMutations, mapGetters } from 'vuex'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {}
+  },
+  created() {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    ...mapState(['inputValue', 'viewKey']),
+    ...mapGetters(['unDoneLength', 'showListByKey'])
+  },
+  methods: {
+    ...mapMutations(['setInputValue', 'addItem', 'removeItem', 'setChecked', 'cleanDoneItem', 'changeViewKey']),
+    // 监听 input 内容的变化
+    handleInputValue(e) {
+      // e.target.value 可拿到输入的最新值
+      this.setInputValue(e.target.value)
+    },
+    // 向列表中新增 item 项
+    addItemToList() {
+      // 判断用户输入是否为空
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('请输入要添加的内容喔')
+      }
+      // 调用追加 newItem 处理函数
+      this.addItem()
+    },
+    // 根据 id，删除 item 项
+    removeItemById(item) {
+      // removeItem 是按需导入使用的
+      this.removeItem(item.id)
+    },
+    // 监听复选框的变化
+    cbStatusChanged(e, item) {
+      // 因为 mutations 里的函数只接收两个参数，所以这里用对象的形式传值
+      const param = {
+        id: item.id,
+        status: e.target.checked
+      }
+      this.setChecked(param)
+    },
+    // 清除已完成
+    cleanDone() {
+      this.cleanDoneItem()
+    },
+    // 切换视图
+    changeList(key) {
+      this.changeViewKey(key)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.body-bg {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  overflow-y: auto;
+  background-color: #1e1e1e;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 80px auto 0;
+  padding: 10px;
+  width: 640px;
+  background-color: #fff;
+  border: 10px solid rosybrown;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,88 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有的任务列表数据
+    list: [],
+    // input 的内容
+    inputValue: '',
+    // 视图的关键字
+    viewKey: 'all'
   },
   getters: {
+    // 统计 done: false 的复选框个数，将统计结果渲染到页面上；只是对数据进行包装展示，因此定义在 getters 节点下。
+    unDoneLength(state) {
+      return state.list.filter((item) => item.done === false).length
+    },
+    // 根据用户点击哪个按钮，来按需展示列表数据
+    showListByKey(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'un_done') {
+        return state.list.filter((item) => !item.done)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter((item) => item.done)
+      }
+      return state.list
+    }
   },
   mutations: {
+    initList(state, res) {
+      state.list = res
+    },
+    // 将 input 输入的值 赋给 state.inputValue
+    setInputValue(state, newValue) {
+      state.inputValue = newValue
+    },
+    // 为 list 列表追加新的 item 项
+    addItem(state) {
+      const newItem = {
+        // 数组的长度刚好就是下一项的 id
+        id: state.list.length,
+        info: state.inputValue,
+        // 事项添加默认未完成，所以为 false
+        done: false
+      }
+      state.list.push(newItem)
+      state.inputValue = ''
+    },
+    // 根据 id 删除对应事项
+    removeItem(state, id) {
+      // 1、将 list 数组中每一项的 id，和传进来的 id，进行比较；返回比较结果，没有匹配的就返回 -1
+      const i = state.list.findIndex((x) => x.id === id)
+      // 2、找到了对应的 id 项，根据 id 来截取数组
+      if (i !== -1) return state.list.splice(i, 1)
+    },
+    // 复选框的变化
+    setChecked(state, param) {
+      const i = state.list.findIndex((x) => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    // 清除已完成的事项
+    cleanDoneItem(state) {
+      state.list = state.list.filter((item) => item.done === false)
+    },
+    // 修改视图按钮的关键字
+    changeViewKey(state, key) {
+      // 将用户点击的视图按钮，对应的 key 值，赋值给 state
+      state.viewKey = key
+    }
   },
   actions: {
+    getList(context) {
+      // json 文件的路径能直接以 / 访问到，是因为 public 文件夹？
+      axios.get('/list.json').then(({ data: res }) => {
+        context.commit('initList', res)
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
